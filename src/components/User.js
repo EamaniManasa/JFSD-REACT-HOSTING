cb0afdf66c221c7191764c94f25baa14972ec69d
EamaniManasa <147276480+EamaniManasa@users.noreply.github.com>
2024-11-13 14:35:25 +0530

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function User(){
     function fun1(){
         console.log(document.getElementsByName("t1")[0].value);
-        axios.get("http://localhost:8080/user",{params:{
+        axios.get("http://jfsd-spring-hosting-production-7082.up.railway.app/user",{params:{
             email:document.getElementsByName("t1")[0].value
         }}).then((res)=>{
             console.log(res.data)
